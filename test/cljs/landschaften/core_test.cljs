2f3d5c2441ec3adfe07d40212544489d58bee9eb
pianostringquartet <cjc500@nyu.edu>
2019-06-18 20:40:08 +0200

    Test file update

--- test/cljs/landschaften/core_test.cljs
+++ test/cljs/landschaften/core_test.cljs
@@ -1,8 +1,12 @@
 (ns landschaften.core-test
-  (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
+  (:require [cljs.test :refer-macros [is are deftest testing use-fixtures run-tests]]
             [pjstadig.humane-test-output]
             [reagent.core :as reagent :refer [atom]]
-            [landschaften.core :as rc]))
+            [landschaften.core :as rc]
+            [day8.re-frame.test :as rf-test]))
+
+
+;(enable-console-print!)
 
 (deftest test-home
   (is (= true true)))
@@ -14,4 +18,11 @@
 ;(def group-with-constraints)
 ;(deftest test->query-constraints)
 
+(deftest test-addition
+  (is (= 2 (+ 1 1))))
 
+
+(deftest test-love
+  (is (= 2 9)))
+
+;(run-tests)
